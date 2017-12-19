e01ea31d14ab2372874d89dd53da37379461e29b
Ewerton Costa <ewertonsjp@gmail.com>
2017-12-19 00:05:14 -0300

    Conecta à API

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-about',
@@ -9,21 +11,44 @@ export class AboutPage {
 
   eventos;
 
-  constructor(public navCtrl: NavController) {
-    this.eventos = [
-      {
-        'autor':'Aislan Rafael',
-        'img':'http://9solucoes.com.br/seminfo/images/palestras/aislan1.jpg',
-        'descricao':'Inovação, empreendedorismo e aceleração de ideias no semiárido.',
-        'likes':0
-      },
-      {
-        'autor':'Rômulo Camara',
-        'img':'http://9solucoes.com.br/seminfo/images/palestras/open.jpg',
-        'descricao':'Desenvolvimento de projetos para a região utilizando hardware livre.',
-        'likes':0
-      }
-    ]
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.get().then(data => {
+      this.eventos = data;
+    });
+  }
+
+  like(evento) {
+    this.post(evento).then(data => {
+      evento.likes += 1;
+    });
+  }
+
+  /**
+   * Busca os eventos cadastrados na API
+   */
+  get() {
+    return new Promise(resolve => {
+      this.http.get("https://seminfo.herokuapp.com/api/events").map(res => res.json()).subscribe(data => {
+        resolve(data.events);
+      });
+    });
+  }
+
+  /**
+   * Dá uma like em um evento na API
+   * @param event 
+   */
+  post(evento) {
+    let body = {
+      
+    }
+    return new Promise(resolve => {
+      this.http.post('https://seminfo.herokuapp.com/api/events/' + evento.id + '/like', body).subscribe(data => {
+         resolve(data);
+      }, error => {
+          console.log("Oooops!");
+      });
+    });
   }
 
 }
